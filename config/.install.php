a254d0251310f45973170f71eecd5dea96f2eacc
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-02-09 17:50:05 +0300

    Файл конфигурации установки виджета

---
+++ config/.install.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Этот файл является частью виджета веб-приложения GearMagic.
+ * 
+ * Файл конфигурации установки виджета.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    'use'         => FRONTEND,
+    'id'          => 'gm.wd.fine_uploader',
+    'category'    => 'widget',
+    'name'        => 'Fine Uploader',
+    'description' => 'File uploading',
+    'namespace'   => 'Gm\Widget\FineUploader',
+    'path'        => '/gm/gm.wd.fine_uploader',
+    'locales'     => ['ru_RU', 'en_GB'],
+    'events'      => [],
+    'required'    => [
+        ['php', 'version' => '8.2'],
+        ['app', 'code' => 'GM CMS']
+    ]
+];
